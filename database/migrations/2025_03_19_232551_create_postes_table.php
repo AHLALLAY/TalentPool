0f3f71592b24743ebff89b35b6b346fa3985a4bc
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-19 23:39:13 +0000

    feat: Ajouter les migrations et les migrer

---
+++ database/migrations/2025_03_19_232551_create_postes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('type');
+            $table->string('description');
+            $table->timestamp('launch_date');
+            $table->timestamp('expired_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('postes');
+    }
+};
