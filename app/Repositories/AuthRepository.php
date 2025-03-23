c9af04aa245ebe1a7d93bc6734b63efe6d3f17fc
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-23 22:45:27 +0000

    chors: Déplacer 'AuthRepositoryInterface' vers 'App\Interfaces'

--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repositories;
 
-use App\Repositories\AuthRepositoryInterface;
+use App\Interfaces\AuthRepositoryInterface;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
