7040be1c1aa14669202ce93a866db8bbf2392bfc
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-23 23:02:33 +0000

    refact: Renommer le namespace OperatorRepositoryInterface

--- app/Interfaces/OperatorRepositoryInterface.php
+++ app/Interfaces/OperatorRepositoryInterface.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Interfaces;
 
-interface OperatorInterface
+interface OperatorRepositoryInterface
 {
     public function addPost();
     public function updatePost($id);
