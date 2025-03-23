00ab5ed31a166099029e444d6595859fa6581a6b
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-23 17:41:07 +0000

    feat: Ajouter l'implemenation des méthodes de l'inteface implemenaté

--- app/Repositories/Auth/AuthRepository.php
+++ app/Repositories/Auth/AuthRepository.php
@@ -1,14 +1,44 @@
 <?php
 
-namespace App;
+namespace App\Repositories\Auth;
 
-class AuthRepository
+use App\Auth\AuthRepositoryInterface;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthRepository implements AuthRepositoryInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function register(array $data)
     {
-        
+        // Créer un nouvel utilisateur
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return $user;
+    }
+
+    public function login(array $data)
+    {
+        // Vérifier les informations d'identification
+        if (!$token = JWTAuth::attempt($data)) {
+            return null; // Retourner null si les informations sont invalides
+        }
+
+        return $token; // Retourner le token JWT
+    }
+
+    public function logout()
+    {
+        // Invalider le token JWT
+        JWTAuth::invalidate(JWTAuth::getToken());
+
+        // Déconnecter l'utilisateur
+        Auth::logout();
+
+        return true;
     }
 }
