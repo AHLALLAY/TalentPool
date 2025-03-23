1fcd6e3861e1c336dd467d58f1c2d7a656f7dbb0
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-23 22:18:50 +0000

    feat: Ajouter la méthode 'login'

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Repositories\AuthRepositoryInterface;
 
 class AuthController extends Controller
 {
-    //
+    protected $authRepository;
+
+    public function __construct(AuthRepositoryInterface $authRepository){
+        $this->authRepository = $authRepository;
+    }
+
+    public function login(LoginRequest $request){   
+        $token = $this->authRepository->login($request->only('email', 'password'));
+
+        if(!$token){
+            return response()->json([
+                "message" => "Unauthorazed"
+            ], 401);
+        }
+
+        return response()->json([
+            "token" => $token,
+            "token_type" => "bearer"
+        ], 200);
+    }
 }
