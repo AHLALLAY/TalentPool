55b7cc37591bb137a2efb217ae875d3c4a43a574
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-23 23:05:54 +0000

    feat: Ajouter OperatorRepository

---
+++ app/Repositories/OperatorRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\OperatorRepositoryInterface;
+
+class OperatorRepository implements OperatorRepositoryInterface
+{
+    public function addPost(){
+        
+    }
+    public function updatePost($id){
+
+    }
+    public function deletePost($id){
+
+    }
+    public function displayMyPosts(){
+
+    }
+    public function displayApplicationOnMyPosts(){
+
+    }
+    public function updateApplicationStatus($id){
+
+    }
+    // statistics
+    public function countMyPosts(){
+
+    }
+    public function PostWithMaxApplication(){
+
+    }
+    public function PostWithMinApplication(){
+
+    }
+}
