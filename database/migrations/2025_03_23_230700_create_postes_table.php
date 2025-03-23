b88466f02c089d60da7831c7134656b0e03d907c
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-23 23:18:11 +0000

    feat: Ajouter le model 'Poste'

--- database/migrations/2025_03_23_230700_create_postes_table.php
+++ database/migrations/2025_03_23_230700_create_postes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->date('expired_date');
-            $table->foreignId('users_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
