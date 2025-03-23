8c43b26b2d60e90ab25c784f27828c816afdd79f
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-23 23:47:31 +0000

    feat: Ajouter l'implementatin de la méthode 'register' dans 'AuthController'

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Interfaces\AuthRepositoryInterface;
 
 class AuthController extends Controller
@@ -12,6 +13,15 @@ class AuthController extends Controller
 
     public function __construct(AuthRepositoryInterface $authRepository){
         $this->authRepository = $authRepository;
+    }
+
+    public function register(RegisterRequest $request){
+        $user = $this->authRepository->register($request->only('name', 'email', 'password', 'roles'));
+        // $user = $this->authRepository->register($request->all());
+        
+        return response()->json([
+            "message" => "User registered successfully"
+        ], 201);
     }
 
     public function login(LoginRequest $request){   
