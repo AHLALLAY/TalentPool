189660f3c85aca63f4c0b19952a79b7471f59b73
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-24 13:40:13 +0000

    feat: Ajouter la route vers la méthode 'addPost'

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PosteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::post('/oparetor/posts', [PosteController::class, 'addPost']);
