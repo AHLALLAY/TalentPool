b131698e0f52f6b450c7803c33d11cbd98357318
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-24 13:38:53 +0000

    fix: Réparer la nomination de la column 'expired_date'

--- app/Models/Poste.php
+++ app/Models/Poste.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Poste extends Model
 {
-    protected $fillable = ['title', 'description', 'eexpired_date', 'user_id'];
+    protected $fillable = ['title', 'description', 'expired_date', 'user_id'];
 }
