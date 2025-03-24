df021c7c9223ea5b9da822525cf53d400cb0a3cb
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-24 00:35:16 +0000

    feat: Ajouter 'PosteRequest'

---
+++ app/Http/Requests/PosteRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PosteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $user = $this->user();
+        return $user && $user->roles == 'Operator' ? true : false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:100'],
+            'description' => ['required', 'string', 'min:20', 'max:1000'],
+            'expired_date' => ['required', 'date'],
+            'user_id' => ['required', 'integer', 'min:1'],
+        ];
+    }
+}
