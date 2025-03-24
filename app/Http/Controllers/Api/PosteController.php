d775b5c57d2da333634875ffed2b44905ef26d76
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-24 00:45:37 +0000

    feat: Ajouter PosteController est les méthode à implementer

---
+++ app/Http/Controllers/Api/PosteController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PosteController extends Controller
+{
+    public function addPost($data){
+
+    }
+    public function updatePost($id){
+
+    }
+    public function deletePost($id){
+
+    }
+    public function displayMyPosts(){
+
+    }
+    public function displayApplicationOnMyPosts(){
+
+    }
+    public function updateApplicationStatus($id){
+
+    }
+    public function addInfo($data){
+
+    }
+    // statistics
+    public function countMyPosts(){
+
+    }
+    public function PostWithMaxApplication(){
+
+    }
+    public function PostWithMinApplication(){
+
+    }
+}
