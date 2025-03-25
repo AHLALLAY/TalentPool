3045c239cd476bb9ebc50a44a91a43cfeda9583e
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-25 14:19:13 +0000

    feat: Ajouter AuthService

---
+++ app/Services/AuthService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Reposotories\Interfaces\AuthRepositoryInterface;
+
+class AuthService
+{
+    protected $authRepository;
+
+    public function __construct(AuthRepositoryInterface $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
+    public function register(array $data)
+    {
+        return $this->authRepository->register($data);
+    }
+
+    public function login(array $credentials)
+    {
+        return $this->authRepository->login($credentials);
+    }
+
+    public function logout()
+    {
+        return $this->authRepository->logout();
+    }
+}
