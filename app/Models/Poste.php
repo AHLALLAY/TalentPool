4a598c2f40b3d5abc6d3a40d6cc69161087e4b82
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-26 11:59:09 +0000

    feat: Ajouter la relation entre les models 'poste' et 'application'

--- app/Models/Poste.php
+++ app/Models/Poste.php
@@ -3,12 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Poste extends Model
 {
     protected $fillable = ['title', 'description', 'expired_date', 'user_id'];
 
-    public function user(){
-        $this->belongsTo(User::class);
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
     }
 }
