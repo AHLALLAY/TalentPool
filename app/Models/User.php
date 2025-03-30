76fa854c674bd9e8d966a541a1048735b29fb915
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-30 23:41:58 +0100

    feat: Ajouter l'implementation du l'interface JWTSubject dans le model 'User'

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = ['name','email','password','roles'];
+    protected $fillable = ['name','email','password','roles',];
+    protected $hidden = ['password','remember_token',];
+    protected function casts(){ return ['email_verified_at' => 'datetime','password' => 'hashed',];}
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function operator() {
+        return $this->hasOne(Operator::class);
+    }
+    
+    public function participant() {
+        return $this->hasOne(Participant::class);
+    }
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function getJWTIdentifier()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
