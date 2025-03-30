fbf115d0e66e6b0a1e083510fa61732a49a08e62
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-30 23:44:19 +0100

    feat: Ajouter la liaison enter les interface et les repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Interfaces\ApplicationInterface;
+use App\Interfaces\AuthInterface;
+use App\Interfaces\InfoInterface;
+use App\Interfaces\PostInterface;
+use App\Repositories\ApplicationRepository;
+use App\Repositories\AuthRepository;
+use App\Repositories\InfoRepository;
+use App\Repositories\PostRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(AuthInterface::class, AuthRepository::class);
+        $this->app->bind(PostInterface::class, PostRepository::class);
+        $this->app->bind(InfoInterface::class, InfoRepository::class);
+        $this->app->bind(ApplicationInterface::class, ApplicationRepository::class);
     }
 
     /**
