129352dcc2e397597066a3b6f1b39eeddd49c645
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-30 23:53:57 +0100

    feat: Ajouter les routes vers les méthode du controlleurs

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,40 @@
 <?php
 
+use App\Http\Controllers\Api\ApplicationController;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\OperatorController;
-use App\Http\Controllers\Api\ParticipantController;
+use App\Http\Controllers\api\OperatorController;
+use App\Http\Controllers\api\ParticipantController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+
+Route::controller(AuthController::class)->group(function() {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+    Route::post('/logout', 'logout');
+});
 
 
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::controller(PostController::class)->group(function(){
+    Route::post('/post', 'addPost');
+    Route::delete('/post/delete/{postId}', 'deletePost');
+    Route::get('/posts/operator/{operatorId}', 'displayMyPosts');
+    Route::get('/posts/all', 'displayAllPosts');
+    Route::get('/posts/all/{operatorId}', 'displayApplicationsOnMyPosts');
+    Route::get('/posts/total/{operatorId}', 'countMyPosts');
+});
 
-Route::post('/operator/info', [OperatorController::class, 'addOperatorInfo'])->name('OperatorInfo');
 
-Route::post('/participant/info', [ParticipantController::class, 'addParticipantInfo'])->name('ParticipantInfo');
-Route::post('/participant/application', [ParticipantController::class, 'addApplication'])->name('Application');
+Route::controller(OperatorController::class)->group(function(){
+    Route::post('/operator/info', 'addInfo');
+});
+
+Route::controller(ParticipantController::class)->group(function(){
+    Route::post('/participant/info', 'addInfo');
+});
+
+Route::controller(ApplicationController::class)->group(function(){
+    Route::post('/applications', 'addApplication');
+    Route::delete('/applications/{applicationId}', 'deleteApplication');
+    Route::get('/applications/posts/operator/{operatorId}', 'countAppOnMyPosts');
+});
