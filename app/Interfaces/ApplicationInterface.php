9057f0678ae443e21230529473811152efc79f1a
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-16 21:12:46 +0100

    feat: Ajouter le parametre '' pour la méthode 'changeStatus'

--- app/Interfaces/ApplicationInterface.php
+++ app/Interfaces/ApplicationInterface.php
@@ -7,7 +7,7 @@ interface ApplicationInterface
     public function addApplication($applicationData); // pour le condidat
     public function deleteApplication($applicationId); // pour le condidat
     public function displayMyApplications($participantId); // pour le condidat
-    public function changeStatus($applicationId); // pour le recruteur
+    public function changeStatus($applicationId, $newStatus); // pour le recruteur
 
     // statistics
     public function countAppOnMyPosts(int $operatorId);  // pour le recruteur
