bb4bd69321cffd3c26bc516322b13a06cb7ae4eb
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-16 20:50:24 +0100

    feat: Ajouter la signature 'changeStatus'

--- app/Interfaces/ApplicationInterface.php
+++ app/Interfaces/ApplicationInterface.php
@@ -7,6 +7,7 @@ interface ApplicationInterface
     public function addApplication($applicationData); // pour le condidat
     public function deleteApplication($applicationId); // pour le condidat
     public function displayMyApplications($participantId); // pour le condidat
+    public function changeStatus($applicationId); // pour le recruteur
 
     // statistics
     public function countAppOnMyPosts(int $operatorId);  // pour le recruteur
